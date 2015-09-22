f5b9763f2e611067152c2d290a31548908c5c938
Thomas Wanielista <tomwans@gmail.com>
2015-09-21 14:19:23 -1000

    RFC3339 test

--- log_test.go
+++ log_test.go
@@ -1,14 +1,26 @@
 package httplog
 
 import (
-	"reflect"
+	"strings"
 	"testing"
+	"time"
 )
 
 func TestLoggingToBuffer(t *testing.T) {
 	l := New()
 	l.Println("hey there")
-	if !reflect.DeepEqual(l.buf.Bytes(), []byte("hey there\n")) {
+	if !strings.HasSuffix(l.buf.String(), "hey there\n") {
 		t.Fatalf("logging to buffer don't work")
+	}
+}
+
+func TestEnsureTimeIsLoggedAsRFC3339(t *testing.T) {
+	l := New()
+	l.Println("ok")
+	x := l.buf.String()
+	xs := strings.Split(x, " ")
+	_, err := time.Parse(time.RFC3339, xs[0])
+	if err != nil {
+		t.Fatalf("expected first non-space characters to be the RFC3339 timestamp: %s", err)
 	}
 }
