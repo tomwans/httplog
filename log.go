daef5c7b2e8960624f92dd73b60eb3172f9222d3
Thomas Wanielista <tomwans@gmail.com>
2015-10-21 00:04:22 -0400

    log compatability.

--- log.go
+++ log.go
@@ -8,8 +8,13 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 	"sync"
 	"time"
+)
+
+const (
+	RFC3339Len = 25
 )
 
 type Logger struct {
@@ -31,22 +36,34 @@ type Logger struct {
 
 // New creates a new instance of the httplog.Logger. All Loggers will
 // log to the same http endpoint.
-func New(prefix string, limit int) *Logger {
+func New(_ io.Writer, prefix string, _ int) *Logger {
+	// create a pool that will always have a buffer preallocated w/
+	// memory.
 	lenPrefix := len(prefix)
-	buf := &bytes.Buffer{}
-	return &Logger{
-		prefix: prefix,
-		buf:    buf,
-		limit:  limit,
-		m:      new(sync.RWMutex),
-		bufPool: &sync.Pool{
-			New: func() interface{} {
-				b := &bytes.Buffer{}
-				b.Grow(25 + 1 + 1 + lenPrefix + 1024)
-				return b
-			},
+	bufPool := &sync.Pool{
+		New: func() interface{} {
+			b := &bytes.Buffer{}
+			// RFC3339Len + Spaces + Prefix + magic number guessing how much we're going to write on average.
+			b.Grow(RFC3339Len + 2 + lenPrefix + 1024)
+			return b
 		},
 	}
+
+	return &Logger{
+		prefix:  prefix,
+		buf:     &bytes.Buffer{},
+		limit:   2048,
+		m:       new(sync.RWMutex),
+		bufPool: bufPool,
+	}
+}
+
+func (l *Logger) SetLimit(limit int) {
+	l.limit = limit
+}
+
+func (l *Logger) Prefix() string {
+	return l.prefix
 }
 
 func (l *Logger) setupBuffer() *bytes.Buffer {
@@ -98,25 +115,6 @@ func (l *Logger) truncateBytes(n int) {
 	}
 }
 
-// Println prints a line to the logger.
-func (l *Logger) Println(v ...interface{}) {
-	b := l.setupBuffer()
-	fmt.Fprintln(b, v...)
-	l.printBuffer(b)
-}
-
-func (l *Logger) Printf(format string, v ...interface{}) {
-	b := l.setupBuffer()
-	fmt.Fprintf(b, format, v...)
-	l.printBuffer(b)
-}
-
-func (l *Logger) Print(v ...interface{}) {
-	b := l.setupBuffer()
-	fmt.Fprint(b, v...)
-	l.printBuffer(b)
-}
-
 func (l *Logger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	l.m.RLock()
 	n, err := io.Copy(w, l.buf)
@@ -126,4 +124,67 @@ func (l *Logger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(fmt.Sprintf("error: only written %d bytes due to: %s", n, err)))
 		return
 	}
+}
+
+// compatability with std log
+
+func (l *Logger) Output(_ int, s string) {
+	l.Println(s)
+}
+
+func (l *Logger) Print(v ...interface{}) {
+	b := l.setupBuffer()
+	fmt.Fprint(b, v...)
+	l.printBuffer(b)
+}
+
+func (l *Logger) Printf(format string, v ...interface{}) {
+	b := l.setupBuffer()
+	fmt.Fprintf(b, format, v...)
+	l.printBuffer(b)
+}
+
+func (l *Logger) Println(v ...interface{}) {
+	b := l.setupBuffer()
+	fmt.Fprintln(b, v...)
+	l.printBuffer(b)
+}
+
+func (l *Logger) Fatal(v ...interface{}) {
+	l.Print(v)
+	os.Exit(1)
+}
+
+func (l *Logger) Fatalf(format string, v ...interface{}) {
+	l.Printf(format, v)
+	os.Exit(1)
+}
+
+func (l *Logger) Fatalln(v ...interface{}) {
+	l.Println(v)
+	os.Exit(1)
+}
+
+func (l *Logger) Panic(v ...interface{}) {
+	b := l.setupBuffer()
+	fmt.Fprint(b, v...)
+	s := b.String()
+	l.printBuffer(b)
+	panic(s)
+}
+
+func (l *Logger) Panicf(format string, v ...interface{}) {
+	b := l.setupBuffer()
+	fmt.Fprintf(b, format, v...)
+	s := b.String()
+	l.printBuffer(b)
+	panic(s)
+}
+
+func (l *Logger) Panicln(v ...interface{}) {
+	b := l.setupBuffer()
+	fmt.Fprintln(b, v...)
+	s := b.String()
+	l.printBuffer(b)
+	panic(s)
 }
